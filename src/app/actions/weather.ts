1c1e4b02916e8a046c5137bb741b71aec00707da
justbenuk <justbenuk@gmail.co.uk>
2024-05-26 22:48:48 +0100

    initial commit

--- src/app/actions/weather.ts
+++ src/app/actions/weather.ts
@@ -14,6 +14,6 @@ export async function getWeather() {
     const response = await fetch(url, options, );
     return response.json()
   } catch (error) {
-    return <p>Sorry! There was an error</p>
+    return console.log('<p>Sorry There was an error</p>')
   }
 }
